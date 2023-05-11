d461afe98e181919a83adda024d443e2c753d9e5
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-11 16:25:09 -0300

    Primeiro commit

---
+++ 10mai/basic.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int numero1, numero2;
+
+  scanf("%d", &numero1);
+
+  scanf("%d", &numero2);
+
+  printf("X = %d", numero1 + numero2);
+
+  printf("\n");
+
+  return 0;
+}
