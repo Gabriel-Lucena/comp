f840b3c635c2d7ead1e9c79ad3e5833d9ca9902e
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2023-05-27 23:00:53 -0300

    Exercício

--- 24mai/macacos.py
+++ 24mai/macacos.py
@@ -19,32 +19,60 @@ for i in range(number_of_trees - 1):
         greatest_tree.pop(1)
         greatest_tree.pop(0)
 
-        greatest_tree.insert(0, i + 2)
+        greatest_tree.insert(0, i + 1)
         greatest_tree.append(int(trees_input[2]))
 
 
-def define_line(ith_tree, greatest_tree):
+def define_line(ith_tree, nth_tree, trees):
 
-    coefficient_a = (greatest_tree[1] - trees[ith_tree]) / (greatest_tree[0] - 1)
-    coefficient_b = trees[ith_tree]
+    coefficient_a = (trees[ith_tree] - trees[nth_tree]) / (ith_tree - nth_tree)
+    coefficient_b = trees[ith_tree] - coefficient_a * ith_tree
 
     line_values = [coefficient_a, coefficient_b]
 
     return line_values
 
 
-first_line = define_line(0, greatest_tree)
-
 ith_tree = 0
 
-while ith_tree != greatest_tree:
+line = define_line(greatest_tree[0], ith_tree, trees)
 
-    if (first_line[0] * ith_tree + first_line[1]) >= trees[ith_tree]:
-        jumps += 1
+jumps += 1
+
+while ith_tree != greatest_tree[0]:
+
+    if ith_tree == 0:
         ith_tree += 1
         continue
+
+    if (line[0] * ith_tree + line[1]) >= trees[ith_tree]:
+        ith_tree += 1
     else:
-        first_line = define_line(ith_tree, greatest_tree)
+        jumps += 1
+
+        if ith_tree != greatest_tree[0]:
+            line = define_line(greatest_tree[0], ith_tree,  trees)
+            ith_tree += 1
 
 
-print(trees, greatest_tree, define_line(trees, greatest_tree))
+line = define_line(len(trees) - 1, greatest_tree[0], trees)
+
+jumps += 1
+
+
+while ith_tree != (len(trees) - 1):
+
+    if ith_tree == greatest_tree[0]:
+        ith_tree += 1
+        continue
+
+    if (line[0] * ith_tree + line[1]) >= trees[ith_tree]:
+        ith_tree += 1
+    else:
+        jumps += 1
+
+        if ith_tree != (len(trees) - 1):
+            line = define_line(len(trees) - 1, ith_tree, trees)
+            ith_tree += 1
+
+print(jumps)
